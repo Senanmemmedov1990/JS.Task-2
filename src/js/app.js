5547e3fa731ee499a2584624cf05fa5deaf153a6
Senanmemmedov1990 <memmedov8665341@gmail.com>
2023-09-21 00:28:21 +0400

    Confirm

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+let sonuc = confirm("NightMode");
+
+
+    if(true){
+        document.body.style.backgroundColor = "black";
+    }
+    else{
+        document.body.style.backgroundColor = "white";
+    }
+
+
